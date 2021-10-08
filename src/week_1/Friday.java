9fea66e03e76aacb29a0b7735597d433667a4de1
m10a25 <aktol54@gmail.com>
2021-10-08 12:31:15 -0400

    Added print statement

--- src/week_1/Friday.java
+++ src/week_1/Friday.java
@@ -2,7 +2,10 @@ package week_1;
 
 public class Friday {
     public static void main(String[] args) {
+
         System.out.println("Friday love");
+        System.out.println("Well, this was fun! Let's do it again sometime next week:)");
+
     }
 
     //some line

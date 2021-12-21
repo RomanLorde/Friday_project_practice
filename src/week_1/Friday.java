566088f6b099e351386b09048699bb96ceb38b27
Shirin Geldiyeva <shirin7271@gmail.com>
2021-12-20 20:53:10 -0500

    Updated friday class one mor

--- src/week_1/Friday.java
+++ src/week_1/Friday.java
@@ -2,9 +2,12 @@ package week_1;
 
 public class Friday {
     public static void main(String[] args) {
-        System.out.println("Friday love");
-    }
 
-    //some line
-    //more line
+        System.out.println("Friday love");
+
+
+        //some line
+        //more line
+        System.out.println("Hi everyone");
+    }
 }
